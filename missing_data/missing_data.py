e8528b99369388b12dfca64368b9e9e9b5f24672
sam <satiricallaughter@gmail.com>
2015-05-04 18:14:32 +0100

    added missing data ratio tool and the ability to replace missing data with the average of neighbouring pixels

--- missing_data/missing_data.py
+++ missing_data/missing_data.py
@@ -4,6 +4,23 @@ values stored in a numpy array."""
 
 import numpy as np
 
+
+"""Missing data is indicated by the value -999 in the IDRISI
+Raster A.1 format."""
+MISSING = -999
+
+def missing_coordinates(data):
+    """Returns an np array of coordinates where data is missing."""
+    missing_mask = (data == MISSING)
+    return np.where(missing_mask == True)
+
+def missing_ratio(data):
+    """Returns the ratio of the number of missing pixels to the 
+    number of total pixels"""
+    missing_mask = (data == MISSING)
+    ratio =  (float(missing_mask.sum()) / data.size)
+    return ratio
+
 def find_pixel_neighbourhood(pixel_loc, pixel_grid_dim):
     """Returns a numpy array of pixels neigbouring the location 
     at 'pixel_loc' in a pixel grid with dimensions 'pixel_grid_dim'.
@@ -23,3 +40,27 @@ def find_pixel_neighbourhood(pixel_loc, pixel_grid_dim):
         down_nbor = pixel_loc + np.array([0, 1]) # shift down
         neighbours['down_nbor'] = down_nbor
     return neighbours
+
+def neighbourhood_average(pixel_loc, neighbours, data):
+    """Calculates pixel_loc with the average of non-missing
+    data in the neighbourhood."""
+    data_count = 0 
+    data_sum = 0
+    for loc in neighbours.values():
+        if data[loc[0], loc[1]] > MISSING:
+            data_count += 1
+            data_sum += data[loc[0], loc[1]]
+    average = int(data_sum / data_count)
+    return average
+
+def replace_missing_values(data):
+    """Takes in a grid of pixels 'data' (as a numpy array) and finds the
+    neighbourhoods of missing pixel data (which are represented by 
+    a value of -999)."""
+    pixel_grid_dim = data.shape
+    missing_coords = missing_coordinates(data)
+    original_data = np.copy(data) 
+    for pixel_loc in zip(missing_coords[0], missing_coords[1]):
+        neighbours = find_pixel_neighbourhood(pixel_loc, pixel_grid_dim)
+        data[pixel_loc] = neighbourhood_average(pixel_loc, neighbours, original_data)
+    return data
--- missing_data/tests/test_missing_data.py
+++ missing_data/tests/test_missing_data.py
@@ -1,14 +1,47 @@
 """Add parent directory to path"""
 import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir_loc = os.path.abspath(inspect.getfile(inspect.currentframe()))
+currentdir = os.path.dirname(currentdir_loc)
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
 import unittest
 import numpy as np
-from missing_data import find_pixel_neighbourhood
+from missing_data import find_pixel_neighbourhood, neighbourhood_average 
+from missing_data import missing_coordinates, replace_missing_values
+from missing_data import missing_ratio
 
-class TestMissingData(unittest.TestCase):
+class TestMissingCoordinates(unittest.TestCase):
+
+	def test_missing_coordinates(self):
+		"""checks that the correct tuple of x_coords and y_coords 
+		of missing data is being returned"""
+		data = np.array([[3,4,5],
+					     [4,-999,-999],
+					     [2,-999,5]])
+		expected_coordinates = (np.array([1, 1, 2]), np.array([1, 2, 1]))
+		coords = missing_coordinates(data)
+		np.testing.assert_array_equal(expected_coordinates, coords)
+
+	def test_missing_data_ratio_with_one_missing_pixel(self):
+		"""check that missing_data_ratio works correctly"""
+		data = np.array([[3,4,5],
+					     [4,-999,3],
+					     [2,2,5]])
+		expected_missing_ratio = 1. / 9
+		ratio = missing_ratio(data)
+		self.assertEqual(expected_missing_ratio, ratio)
+
+	def test_missing_data_ratio_with_multiple_missing_pixels(self):
+		"""check that missing_data_ratio works correctly"""
+		data = np.array([[3,4,5],
+					     [4,-999,-999],
+					     [-999,2,5]])
+		expected_missing_ratio = 1. / 3
+		ratio = missing_ratio(data)
+		self.assertEqual(expected_missing_ratio, ratio)
+
+class TestMissingDataNeighbourhoods(unittest.TestCase):
 
 	def assert_dict_values_of_numpys_equal(self, dict1, dict2):
 		"""compares the set of numpy arrays forming the values 
@@ -83,6 +116,68 @@ class TestMissingData(unittest.TestCase):
 		neighbours = find_pixel_neighbourhood(pixel_loc, pixel_grid_dim)
 		self.assertEqual(expected_neighbours.keys(), neighbours.keys())
 		self.assert_dict_values_of_numpys_equal(expected_neighbours, neighbours)
+
+class TestDataAveraging(unittest.TestCase):
+
+	def test_complete_neighbourhood_average(self):
+		"""check neighbourhood average is correct when no neighbours contain 
+		missing data."""
+		data = np.array([[3,4,5],
+					     [4,-999,6],
+					     [2,8,5]])
+		pixel_loc = np.array([1,1])
+		neighbours = {
+			'left_nbor': np.array([0, 1]),
+			'right_nbor': np.array([2, 1]),
+			'up_nbor': np.array([1, 0]),
+			'down_nbor': np.array([1, 1])
+		}
+		expected_average = int(5.5) # All values must be integers
+		average = neighbourhood_average(pixel_loc, neighbours, data)
+		self.assertEqual(expected_average, average)
+
+	def test_partial_neighbourhood_average(self):
+		"""check neighbourhood average is correct when some neighbours contain 
+		missing data."""
+		data = np.array([[3,4,5],
+					     [4,-999,-999],
+					     [2,-999,5]])
+		pixel_loc = np.array([1,1])
+		neighbours = {
+			'left_nbor': np.array([0, 1]),
+			'right_nbor': np.array([2, 1]),
+			'up_nbor': np.array([1, 0]),
+			'down_nbor': np.array([1, 1])
+		}
+		expected_average = 4
+		average = neighbourhood_average(pixel_loc, neighbours, data)
+		self.assertEqual(expected_average, average)
+
+class TestDataReplacement(unittest.TestCase):
+
+	def test_single_missing_value_replacement(self):
+		"""check neighbourhood average is correct when no neighbours contain 
+		missing data."""
+		missing_data = np.array([[3,4,5],
+						         [4,-999,6],
+						         [2,8,5]])
+		expected_data = np.array([[3,4,5],
+					     		  [4,5,6],
+					     		  [2,8,5]])
+		data = replace_missing_values(missing_data)
+		np.testing.assert_array_equal(expected_data, data)
+
+	def test_multiple_missing_value_replacement(self):
+		"""check neighbourhood average is correct when no neighbours contain 
+		missing data."""
+		missing_data = np.array([[3,4,5],
+						         [4,-999,-999],
+						         [2,-999,5]])
+		expected_data = np.array([[3,4,5],
+					     		  [4,4,5],
+					     		  [2,3,5]])
+		data = replace_missing_values(missing_data)
+		np.testing.assert_array_equal(expected_data, data)
 		
 if __name__ == "__main__":
 	unittest.main()
